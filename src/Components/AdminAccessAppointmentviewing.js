6f552b3a6ac57e3065d37d440171ed5d418be4e6
armela1 <128065866+armela1@users.noreply.github.com>
2023-05-09 00:30:04 +0100

    Update AdminAccessAppointmentviewing.js

--- src/Components/AdminAccessAppointmentviewing.js
+++ src/Components/AdminAccessAppointmentviewing.js
@@ -3,21 +3,27 @@ import { InsetText, Input, Button } from 'govuk-react';
 import "./AdminAccessAppointmentviewing";
 
 function AdminAccessAppointmentviewing() {
+    // Define state variables for doctorId using useState hook
     const [doctorId, setDoctorId] = useState('');
 
+    // Define a function to handle form submission
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(`Doctor ID: ${doctorId}`);
     };
 
 
+    // Render the component
     return (
         <>
             <h1>Admin Access Appointment viewing</h1>
+            {/* Create a form and attach the handleSubmit function to the onSubmit event */}
             <form onSubmit={handleSubmit}>
+                {/* Display a text message */}
                 <InsetText>
                     Enter a doctor id to search upcoming appointments
                 </InsetText>
+                {/* Label and input for doctorId */}
                 <label htmlFor="admin-id">Doctor ID</label>
                 <Input
                     className="govuk-input"
@@ -27,7 +33,9 @@ function AdminAccessAppointmentviewing() {
                     value={doctorId}
                     onChange={(e) => setDoctorId(e.target.value)}
                 />
+                {/* Submit button */}
                 <Button type="submit">Continue</Button>
+                {/* Reset button */}
                 <Button type="reset">Back</Button>
             </form>
         </>
