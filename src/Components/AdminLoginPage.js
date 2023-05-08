3f43dc76000ee6598ada4cd199b433d73419de33
armela1 <128065866+armela1@users.noreply.github.com>
2023-05-09 00:27:51 +0100

    Update AdminLoginPage.js

--- src/Components/AdminLoginPage.js
+++ src/Components/AdminLoginPage.js
@@ -3,19 +3,23 @@ import {Input, Button} from 'govuk-react';
 import './AdminLoginPage.css';
 
 function AdminLoginPage() {
+  // Define state variables for adminId and password using useState hook
   const [adminId, setAdminId] = useState('');
   const [password, setPassword] = useState('');
 
+  // Define a function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(`Admin ID: ${adminId}, Password: ${password}`);
   };
 
+  // Render the component
   return (
     <><h1>Admin Login</h1>
       <div className="AdminLoginPage-form">
-      
+      {/* Create a form and attach the handleSubmit function to the onSubmit event */}
         <form onSubmit={handleSubmit}>
+          {/* Label and input for adminId */}
           <label htmlFor="admin-id">Admin ID</label>
         <Input
           id="admin-id"
@@ -23,6 +27,7 @@ function AdminLoginPage() {
           value={adminId} 
           onChange={(Event) => setAdminId(Event.target.value)} 
           />
+          {/* Label and input for password */}
           <label htmlFor="admin-id">Password</label>
           <Input
             id="password"
@@ -31,7 +36,9 @@ function AdminLoginPage() {
             value={password}
             onChange={(Event) => setPassword(Event.target.value)} 
             />
+            {/* Submit button */}
             <Button type="submit">Continue</Button>
+            {/* Reset button */}
             <Button type="reset">Back</Button>
           </form>
           </div>
