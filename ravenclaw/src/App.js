f285d8e70b168a99f7546079d08fc48a3d20c2d7
asmaajemal <asmaajxx@gmail.com>
2023-05-09 04:55:24 +0100

    I added anatol Page nav.

--- ravenclaw/src/App.js
+++ ravenclaw/src/App.js
@@ -1,4 +1,5 @@
-
+import DeReg from './Components/DeReg';
+import './Components/gov.francesco.css';
 import './Asma.css';
 import { Page} from 'govuk-react'
 import {Routes, Route} from 'react-router-dom'
@@ -8,7 +9,10 @@ import Patient_gprecord from './Patient_gprecord';
 import React from 'react';
 import Header from './Topnav';
 import MainPage from './main_page';
+import Patientreg from './Components/Patientreg';
 
+import { createContext } from 'react';
+export const CurrentContext = createContext(null);
 
 
 
@@ -19,7 +23,6 @@ function App() {
   return (
     <div className="App">
       <div>
-
       <Page header={<Header></Header>}>
         <Routes>
           <Route path='/'/>
@@ -27,10 +30,15 @@ function App() {
           <Route path='/patient_login' element={<Login/>}/>
           <Route path='/patient_PA' element={<Patient_AP/>}/>
           <Route path='/patient_gprecord' element={<Patient_gprecord/>}/>
+          <Route path='/Patientreg' element={<Patientreg />} />
+          <Route path='/PatientRegNHS' element={<PatientRegNHS/>}/>
+          <Route path='/DeReg' element={<DeReg/>}/>
         </Routes>
       </Page>
       </div>
     </div>
+
+  
   );
 }
 
